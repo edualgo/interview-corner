85e5d913f3d3447b299d907a3c2f544eacdee204
vaishnavi jha <vaishnavi.jha.poorna@gmail.com>
2021-01-06 19:13:21 +0530

    Create painters_partition.cpp

---
+++ Contributor Corner/vaishnavi/painters_partition.cpp
@@ -0,0 +1,73 @@
+// link :https://www.interviewbit.com/problems/painters-partition-problem/
+/*problem - statement 
+Given 2 integers A and B and an array of integars C of size N.
+
+Element C[i] represents length of ith board.
+
+You have to paint all N boards [C0, C1, C2, C3 … CN-1]. There are A painters available and each of them takes B units of time to paint 1 unit of board.
+
+Calculate and return minimum time required to paint all boards under the constraints that any painter will only paint contiguous sections of board.
+
+2 painters cannot share a board to paint. That is to say, a board
+cannot be painted partially by one painter, and partially by another.
+A painter will only paint contiguous boards. Which means a
+configuration where painter 1 paints board 1 and 3 but not 2 is
+invalid.
+*/
+
+//solution by binary search algorithm
+#include<bits/stdc++.h>
+using namespace std;
+bool isPossible(int mid, vector<int> &c, int a) {
+    long long Time = 0, numPainters = 1, n = c.size();
+    for(int i = 0; i < n; i++) {
+        Time += c[i];
+        if(Time > mid) {
+            Time = c[i];
+            numPainters++;
+            if(numPainters > a) return false;
+        }
+    }
+    return true;
+}
+
+int paint(int A, int B, vector<int> &C) {
+    int n = C.size();
+    long long maxTime = 0;
+    long long lo = 0, hi, mid, ans;
+    for(int i = 0; i < n; i++){
+        maxTime += C[i];
+        lo = max(lo,(long long)C[i]);
+    } 
+    hi = maxTime;
+    while(lo <= hi) {
+        mid = lo + (hi - lo) / 2;
+        if(isPossible(mid, C, A)) {
+            ans = mid;
+            hi = mid - 1;
+        }
+        else {
+            lo = mid + 1;
+        }
+    }
+    return (ans * B) % 10000003;
+}
+int main() 
+{ 
+    int n;
+    cout<<"enter size of array"<<endl;
+    cin>>n;
+    vector<int>arr;
+    cout<<"enter elements of array"<<endl;
+    for(int i=0;i<n;i++) {
+    	int a;
+    	cin>>a;
+    	arr.push_back(a);
+	}
+   
+    int k = 3; 
+    int t=paint(k,1,arr);
+    cout<<t<<endl;
+  
+    return 0; 
+} 
