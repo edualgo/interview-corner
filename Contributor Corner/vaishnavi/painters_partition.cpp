8512cc039148e45763d8720801c175488b08eff2
vaishnavi jha <39922632+vaishnavijha@users.noreply.github.com>
2021-01-10 17:43:18 +0530

    Update painters_partition.cpp

--- Contributor Corner/vaishnavi/painters_partition.cpp
+++ Contributor Corner/vaishnavi/painters_partition.cpp
@@ -13,6 +13,27 @@ cannot be painted partially by one painter, and partially by another.
 A painter will only paint contiguous boards. Which means a
 configuration where painter 1 paints board 1 and 3 but not 2 is
 invalid.
+
+Sample test cases:
+Input 1:
+    A = 2
+    B = 5
+    C = [1, 10]
+Output 1:
+    50
+Explanation 1:
+    Possibility 1:- same painter paints both blocks, time taken = 55units
+    Possibility 2:- Painter 1 paints block 1, painter 2 paints block 2, time take = max(5, 50) = 50
+    There are no other distinct ways to paint boards.
+    ans = 50%10000003
+
+Input 2:
+    A = 10
+    B = 1
+    C = [1, 8, 11, 3]
+Output 2:
+    11
+    
 */
 
 //solution by binary search algorithm
