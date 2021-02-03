93edd8ff976a627ba80ec51e3a36aef2f5c1c06d
Ravi Kumar <ravi14577@gmail.com>
2021-02-03 10:54:54 +0530

    solutoin og max data node solution of issue #issue 63 (#72)

---
+++ Contributor Corner/Ravi kumar/Generic tree/solution of max node tree.h
@@ -0,0 +1,82 @@
+
+TreeNode<int>* maxDataNode(TreeNode<int>* root) {
+    /* Don't write main().
+     * Don't read input, it is passed as function argument.
+     * Return output and don't print it.
+     * Taking input and printing output is handled automatically.
+     */
+ if(root==NULL)
+        return NULL;
+    
+    TreeNode <int>* cur=root;
+    
+    for(int i=0;i<root->children.size();i++){
+        
+        TreeNode<int>* desc=maxDataNode(root->children[i]);
+        
+        if(cur->data<desc->data)
+            cur=desc;
+        
+    }
+    return cur;
+
+}
+
+
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    TreeNode<int>* ans = maxDataNode(root);
+
+    if (root != NULL) {
+        cout << ans->data;
+    }
+}
+
